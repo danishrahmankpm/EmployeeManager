7c606c3122271db71eab859bf51479f685e7682e
danishrahmankpm <140050542+danishrahmankpm@users.noreply.github.com>
2025-07-09 16:48:30 +0530

    intermmediate commits

--- src/main/java/com/example/EmployeeManager/Controller/EmployeeNameIdDto.java
+++
@@ -1,5 +0,0 @@
-package com.example.EmployeeManager.Controller;
-
-public class EmployeeNameIdDto {
-
-}
--- src/main/java/com/example/EmployeeManager/Service/DepartmentService.java
+++ src/main/java/com/example/EmployeeManager/Service/DepartmentService.java
@@ -1,6 +1,24 @@
 package com.example.EmployeeManager.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.EmployeeManager.Mapper.DepartmentMapper;
+import com.example.EmployeeManager.Mapper.EmployeeMapper;
+import com.example.EmployeeManager.Repository.DepartmentRepository;
+import com.example.EmployeeManager.Repository.EmployeeRepository;
+
+@Service
 public class DepartmentService {
+    @Autowired
+    DepartmentRepository departmentRepository;
+    @Autowired
+    EmployeeRepository employeeRepository;
+    @Autowired
+    DepartmentMapper departmentMapper;
+    @Autowired
+    EmployeeMapper employeeMapper;
+
     
     
 }
